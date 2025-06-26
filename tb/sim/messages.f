328fc7cde777f6a7dae3718d9d3a618a4834434b
Dhanwanth1810 <Dhanwanth.vb@vaaluka.com>
2025-06-26 18:12:23 +0530

    completed course and all default tests

--- tb/sim/messages.f
+++ tb/sim/messages.f
@@ -3,7 +3,7 @@
 //+uvm_set_verbosity=*,RX_FIFO,UVM_MEDIUM,time,0
 
 +uvm_set_verbosity=*,REG_PREDICT,UVM_HIGH,time,0 
-//+uvm_set_verbosity=*md_*agent.monitor*,ITEM_END,UVM_LOW,time,0
++uvm_set_verbosity=*md_*agent.monitor*,ITEM_END,UVM_LOW,time,0
 
 +uvm_set_verbosity=*,RX_FIFO,UVM_HIGH,time,0 
 +uvm_set_verbosity=*,TX_FIFO,UVM_HIGH,time,0
